372c79180d6ed529af6154eb3162bd9232ad799c
Samuel Rodrigues <rodrigues.samuel98@gmail.com>
2022-09-26 16:58:57 -0300

    teste

---
+++ C#/Meu Primeiro Programa C#/Meuprimeiroprograma - CSharp/Meuprimeiroprograma - CSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿const string textoolamundo = "Olá mundo, esse é o meu primeiro programa C#! Feito por ";
+string nomecompleto;
+int idade;
+
+Console.WriteLine("Qual o seu nome completo?");
+
+nomecompleto = Console.ReadLine().ToString();
+
+Console.WriteLine("Qual sua idade?");
+
+idade = Convert.ToInt32(Console.ReadLine());
+
+bool valido = true;
+
+Console.WriteLine(valido);
+Console.WriteLine(valido);
+
+//Console.WriteLine(textoolamundo + nomecompleto + " que tem " + idade + " anos");
